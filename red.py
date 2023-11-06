52abef51ea8cbb8932ccc7739f62c35024404d2e
Mich <michaenrangelmr@gmail.com>
2023-11-06 13:31:48 -0500

    first commit

---
+++ red.py
@@ -0,0 +1,39 @@
+import spacy
+""" import sys """
+
+def preprocess_text(text):
+    # Aqui cargamos el modelo spañol de Spacy
+    nlp = spacy.load('es_core_news_sm')
+
+    # Se definen los STOP WORDS excluyendo los prononmbres y terminos a utilizar 
+    stop_words = set(['además', 'ante', 'cabe', 'e', 'a', 'el', 'hacia', 'lo'
+                      ,'según','tras','le','es','.', ','])
+
+    # POS tags permitidos (Pronombres, Verbos, Adjetivos)
+    allowed_tags = ['PRON', 'VERB', 'ADJ']
+
+    # Procesar el texto con Spacy
+    doc = nlp(text)
+
+    processed_text = []
+
+    # Iterar los tokens en el texto procesado
+    for token in doc:
+        # Verificar si el token no es una STOP WORD (excluyendo los pronombres) y si el POS tag se encuentra en los tags permitidos
+        if token.text.lower() not in stop_words or token.pos_ in allowed_tags:
+            # Lemmatize the token
+            if token.ent_type_ == 'PER':
+                # Spell out the name character by character and add it to the processed text
+                spelled_name = list(token.text)
+                processed_text.extend(spelled_name)
+            else:
+                # Lemmatize the token and add it to the processed text
+                lemma = token.lemma_
+                processed_text.append(lemma)
+
+    return processed_text
+
+""" # Ejemplo
+text = sys.argv[1]
+processed_text = preprocess_text(text)
+print(processed_text) """
