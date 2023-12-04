a3b8a7c7946184b0135aae5e5fb736ce548ba98e
Mich <michaenrangelmr@gmail.com>
2023-12-04 14:37:03 -0500

    Precise

--- red.py
+++ red.py
@@ -2,11 +2,11 @@ import spacy
 
 def preprocess_text(text):
     # Aqui cargamos el modelo spañol de Spacy
-    nlp = spacy.load('es_core_news_sm') # Modelo Eficiente
-    #nlp = spacy.load('es_dep_news_trf') # Modelo Preciso
+    #nlp = spacy.load('es_core_news_sm') # Modelo Eficiente
+    nlp = spacy.load('es_dep_news_trf') # Modelo Preciso
 
     # Se definen los STOP WORDS excluyendo los prononmbres y terminos a utilizar 
-    stop_words = set(['además','ante', 'cabe','en', 'e', 'a', 'el', 'hacia', 'lo'
+    stop_words = set(['además','ante', 'cabe','en', 'e','las','los', 'a', 'el', 'de', 'ni', 'hacia', 'lo'
                       ,'según','tras','le','al','es','.', ','])
 
     # POS tags permitidos (Pronombres, Verbos, Adjetivos)
@@ -22,11 +22,11 @@ def preprocess_text(text):
         # Verificar si el token no es una STOP WORD (excluyendo los pronombres) y si el POS tag se encuentra en los tags permitidos
         if token.text.lower() not in stop_words or token.pos_ in allowed_tags:
             # Lemmatize the token
-            if token.ent_type_ == 'PER':
+            #if token.ent_type_ == 'PER':
                 # Spell out the name character by character and add it to the processed text
-                spelled_name = list(token.text)
-                processed_text.extend(spelled_name)
-            else:
+                #spelled_name = list(token.text)
+                #processed_text.extend(spelled_name)
+            #else:
                 # Lemmatize the token and add it to the processed text
                 lemma = token.lemma_
                 processed_text.append(lemma)
